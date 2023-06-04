0b845f4fb5a508b2d9c78c18049c1a3a6ca9d022
gitanjali <gmehta0106@gmail.com>
2023-06-03 22:08:49 -0400

    added comments according to Naveen TL

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,9 +1,13 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
 	public static void main(String[] args) {
 		System.out.println("Cart page");
+		By fn = By.xpath(null);
+		
 
 	}
 
